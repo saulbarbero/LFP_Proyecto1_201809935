5aab42205c1b144966cb299364dfbd89a37ad4de
saulbarbero <82236139+saulbarbero@users.noreply.github.com>
2021-09-04 22:07:50 -0600

    Main y entrada

---
+++ Main.py
@@ -0,0 +1,20 @@
+
+
+if __name__ == "__main__":
+  
+    print("Bienvenido")
+    opcion = int(input("Elije una opcion:"))
+
+    
+
+    while opcion != 6:
+
+        if opcion == 1:
+           pass
+
+        elif opcion == 2: 
+            pass
+            
+        else:
+            print("Ingrese una opcion valida")
+        opcion = int(input("Elije una opcion:"))
