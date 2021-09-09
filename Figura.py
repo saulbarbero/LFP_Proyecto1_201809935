1be5235adb78543862016b423f4b9168a075a4f8
saulbarbero <82236139+saulbarbero@users.noreply.github.com>
2021-09-09 17:58:25 -0600

    Clase figura y Nodo

---
+++ Figura.py
@@ -0,0 +1,9 @@
+
+class Figura:
+    def __init__(self):
+        self.titulo = ''
+        self.ancho = 0
+        self.alto = 0
+        self.n = 0 #Rows
+        self.m = 0 #Colum
+        self.filtro = ''
---
+++ NodoSimple.py
@@ -0,0 +1,5 @@
+class NodoSimple:
+    def __init__(self,siguiente=None,data= None):
+        
+        self.siguiente = siguiente
+        self.data = data
